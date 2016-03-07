4bce2c6d212a12a472c8c819d3e5d28574594c10
zyumbik <gleb.sabirzyanov@gmail.com>
2016-03-07 19:26:46 +1000

    Calculator toolbar action menu in progress

--- app/src/main/java/com/developer/zyumbik/goclayout/Calculator.java
+++ app/src/main/java/com/developer/zyumbik/goclayout/Calculator.java
@@ -3,6 +3,7 @@ package com.developer.zyumbik.goclayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class Calculator extends AppCompatActivity {
 
@@ -12,6 +13,14 @@ public class Calculator extends AppCompatActivity {
 		setContentView(R.layout.activity_calculator);
 		Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
 		setSupportActionBar(myToolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.calculator, menu);
+		return true;
 	}
 
 }
