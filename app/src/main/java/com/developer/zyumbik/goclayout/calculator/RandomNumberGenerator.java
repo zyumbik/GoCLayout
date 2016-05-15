1b06bba5e5f29c1e9775e21994608e2319716b7f
zyumbik <gleb.sabirzyanov@gmail.com>
2016-05-15 11:29:23 +1000

    Random number generator

--- app/src/main/java/com/developer/zyumbik/goclayout/calculator/RandomNumberGenerator.java
+++ app/src/main/java/com/developer/zyumbik/goclayout/calculator/RandomNumberGenerator.java
@@ -10,10 +10,12 @@ import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -24,16 +26,19 @@ import java.util.Random;
 public class RandomNumberGenerator extends AppCompatDialogFragment {
 
 	Toolbar toolbar;
-	TextInputEditText limit, output;
+	TextInputEditText output;
+	EditText limit;
 	SeekBar slider;
 	Button button;
 	Random rnd = new Random();
+	boolean userInteraction = false;
+	int currentLimit;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_random_number_generator, container);
 		toolbar = (Toolbar)v.findViewById(R.id.fragment_random_number_toolbar);
-		limit = (TextInputEditText) v.findViewById(R.id.fragment_random_number_limit);
+		limit = (EditText) v.findViewById(R.id.fragment_random_number_limit);
 		output = (TextInputEditText) v.findViewById(R.id.fragment_random_number_output);
 		slider = (SeekBar) v.findViewById(R.id.fragment_random_number_slider);
 		button = (Button) v.findViewById(R.id.fragment_random_number_button_generate);
@@ -55,7 +60,12 @@ public class RandomNumberGenerator extends AppCompatDialogFragment {
 		button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				output.setText(String.valueOf(rnd.nextInt(Integer.valueOf(limit.getText().toString()))));
+				currentLimit = Integer.valueOf(limit.getText().toString());
+				if (currentLimit != Integer.MAX_VALUE) {
+					output.setText(String.valueOf(rnd.nextInt(currentLimit + 1)));
+				} else {
+					output.setText(String.valueOf(rnd.nextInt(currentLimit)));
+				}
 			}
 		});
 	}
@@ -69,14 +79,17 @@ public class RandomNumberGenerator extends AppCompatDialogFragment {
 
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if (!userInteraction) {
+					if (Long.valueOf(s.toString()) > Integer.MAX_VALUE) {
+						limit.setText(String.valueOf(Integer.MAX_VALUE));
+						Toast.makeText(getContext(), "Isn't it too much for you?", Toast.LENGTH_SHORT).show();
+					} else {
+						int a = binlog(Integer.valueOf(limit.getText().toString()) + 1);
+						slider.setProgress(a);
+					}
+				}
 				limit.requestLayout();
 				slider.requestLayout();
-				if (Double.valueOf(s.toString()) > Integer.MAX_VALUE) {
-					limit.setText(String.valueOf(Integer.MAX_VALUE));
-					Toast.makeText(getContext(), "Isn't it too much for you?", Toast.LENGTH_SHORT).show();
-				} else {
-					slider.setProgress((int)Math.log(Double.valueOf(limit.getText().toString())));
-				}
 			}
 
 			@Override
@@ -91,24 +104,33 @@ public class RandomNumberGenerator extends AppCompatDialogFragment {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				if (fromUser) {
-					if (progress == 31) {
-						limit.setText(String.valueOf((int)Math.pow(2, progress - 1) - 1));
+					if (progress == 32) {
+						limit.setText(String.valueOf(Integer.MAX_VALUE));
 					} else {
-						limit.setText(String.valueOf((int)Math.pow(2, progress - 1)));
+						limit.setText(String.valueOf((int)Math.pow(2, progress)));
 					}
 				}
 			}
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
-
+				userInteraction = true;
 			}
 
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
-
+				userInteraction = false;
 			}
 		});
+	}
+
+	public static int binlog( int bits ) {
+		int log = 0;
+		if( ( bits & 0xffff0000 ) != 0 ) { bits >>>= 16; log = 16; }
+		if( bits >= 256 ) { bits >>>= 8; log += 8; }
+		if( bits >= 16  ) { bits >>>= 4; log += 4; }
+		if( bits >= 4   ) { bits >>>= 2; log += 2; }
+		return log + ( bits >>> 1 );
 	}
 
 	private void setToolbar() {
